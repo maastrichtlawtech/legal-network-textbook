19c2da757203835817856e28a5757344f8e58874
Carlos <carloseam94@gmail.com>
2025-02-04 15:48:54 +0100

    chore: create project

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Pdf from './components/Pdf.vue'
+</script>
+
+<template>
+  <Pdf/>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Pdf.vue
@@ -0,0 +1,9 @@
+<template>
+    pdf
+</template>
+<script setup lang="ts">
+
+</script>
+<style scoped>
+
+</style>
