2d1b8badec7a710f3aaa2730f2945e27ad8af915
Carlos <carloseam94@gmail.com>
2025-02-05 12:13:14 +0100

    feat: render ipynb files as html

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
-<script setup lang="ts">
-import Pdf from './components/Pdf.vue'
-</script>
-
 <template>
   <Pdf/>
 </template>
-
+<script setup lang="ts">
+import Pdf from './components/Pdf.vue'
+</script>
 <style scoped>
 </style>
--- src/components/Pdf.vue
+++ src/components/Pdf.vue
@@ -1,8 +1,32 @@
 <template>
-    pdf
+    <div id="pdf-container">
+        <div v-for="nb in notebooks">
+            <div v-html="nb"></div>
+        </div>
+    </div>
 </template>
 <script setup lang="ts">
+import { onMounted, ref } from "vue";
 
+declare let ipynb2html: any;
+const notebooks = ref<string[]>([]);
+
+const loadNotebooks = async () => {
+    const configFile = await fetch('/nbconfig.json');
+    const config = await configFile.json();
+    const notebooksNames = config.notebooks;
+    for(let i = 0; i < notebooksNames.length; ++i) {
+        const nb: string = notebooksNames[i];
+        const file = await fetch(`/notebooks/${nb}.ipynb`);
+        var json = await file.json();
+        var rendered = ipynb2html.render(json);
+        notebooks.value.push(rendered.outerHTML);
+    }
+};
+
+onMounted(async () => {
+    await loadNotebooks();
+})
 </script>
 <style scoped>
 
