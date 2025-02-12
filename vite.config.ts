79f14fd73ec1c6912848289ba05148ea6741e539
Carlos <carloseam94@gmail.com>
2025-02-12 11:29:42 +0100

    chore: remove tailwind from dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue()],
   esbuild: {
     target: 'es2022' // Explicitly set the target for esbuild
   }
